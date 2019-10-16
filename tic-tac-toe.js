2e2fd67938cdf4ddd54c3373226617e260250330
NateChris <divineconclusion@gmail.com>
2019-10-16 12:52:12 -0500

    Let person click on square and set X or O

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -1,6 +1,23 @@
+let playerTurn = 'X';
+
 window.onload = function () {
     let boardDiv = document.getElementById("board");
     for (let ele of boardDiv.children) {
         ele.className = 'square';
+        ele.addEventListener('click', function() {squareSelect(ele)});
     }
 }
+
+function squareSelect(ele) {
+    let eleCl = ele.classList
+    if (!(eleCl.contains('X') || eleCl.contains('O'))) {
+        eleCl.add(playerTurn);
+        ele.innerHTML = playerTurn;
+        if (playerTurn === 'X') {
+            playerTurn = 'O';
+        } else {
+            playerTurn = 'X';
+        }
+    }
+}
+
